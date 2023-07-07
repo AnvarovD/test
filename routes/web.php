1eb53bd63fbf7c0af378b9eaf84213a25e2929ac
AnvarovD <doniyer.anvarov@alif.uz>
2023-07-07 11:53:57 +0500

    fix

--- routes/web.php
+++ routes/web.php
@@ -15,26 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-//    $fp = fsockopen("localhost", 50000, $errno, $errstr, 30);
-//    if (!$fp) {
-//        echo "$errstr ($errno)<br />\n";
-//    } else {
-//        fwrite($fp, json_encode(["message" => "The string you want to send"]));
-////        while (fgets($fp, 128)) {
-////            echo fgets($fp, 128); // If you expect an answer
-////        }
-//        fclose($fp); // To close the connection
-//    }
-
-    return "ok";
-})->middleware(RouteLogMiddleware::class);
-
-Route::prefix('logstash')
-    ->get('/polylog', static function () {
-       $d = \Illuminate\Support\Facades\Http::baseUrl("https://jsonplaceholder.typicode.com")
-            ->withMiddleware(new \Alifuz\Polylog\Middlewares\GuzzleLogMiddleware("test", [], 'logstash'))
-            ->get("/todos/1")->json();
-        return $d;
-    })
-//->middleware(RouteLogMiddleware::class)
-;
+    return redirect('/admin');
+});
